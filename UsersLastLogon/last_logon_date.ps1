80aac92e5ff910411537bb68c258d4d30c420605
Jorge Pla <j.pla88@hotmail.com>
2016-10-23 18:35:30 -0400

    Add files via upload

---
+++ UsersLastLogon/last_logon_date.ps1
@@ -0,0 +1,6 @@
+﻿$Users = Get-ADUser -filter {Enabled -eq $True -and Memberof -eq "CN=MacPro Users,OU=Groups,DC=contoso,DC=com"} `
+-SearchBase "OU=Chicago,DC=contoso,DC=com" –Properties “DisplayName”, "lastLogonTimestamp" |
+Select-Object -Property “Displayname”, @{Name=“LastLogon”;Expression={[datetime]::FromFileTime($_.“lastLogonTimestamp”).ToString("MM/dd/yyyy")}} 
+
+
+$Users | Export-Csv 'C:\Users\username\Desktop\Users.csv' -NoTypeInformation
