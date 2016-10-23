4b77de09ed23c2aaee74059dd60d19b0e29eae03
Jorge Pla <j.pla88@hotmail.com>
2016-10-23 18:35:50 -0400

    Update last_logon_date.ps1

--- UsersLastLogon/last_logon_date.ps1
+++ UsersLastLogon/last_logon_date.ps1
@@ -2,5 +2,4 @@
 -SearchBase "OU=Chicago,DC=contoso,DC=com" –Properties “DisplayName”, "lastLogonTimestamp" |
 Select-Object -Property “Displayname”, @{Name=“LastLogon”;Expression={[datetime]::FromFileTime($_.“lastLogonTimestamp”).ToString("MM/dd/yyyy")}} 
 
-
 $Users | Export-Csv 'C:\Users\username\Desktop\Users.csv' -NoTypeInformation
